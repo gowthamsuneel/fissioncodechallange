7f9c3ed9a6b9ffe0dd8c570e692394dd8e12d470
gowthamsuneel <suneelgowtham@gmail.com>
2017-04-02 18:18:34 +0530

    Changed naming convenstions

--- SachinBestandWorstScore.js
+++ SachinBestandWorstScore.js
@@ -1,6 +1,6 @@
 var seriesScores=[10,5,20,20,4,5,2,25,1];
 var highestScore=seriesScores[0], lowestScore=seriesScores[0];
-var highestScoreCount=0, lowestScoreCount=0;
+var mostRecordRuns=0, leastRecordRuns=0;
 
 
 for (var i = 1; i <seriesScores.length; i++) {
@@ -9,16 +9,16 @@ for (var i = 1; i <seriesScores.length; i++) {
 	if(highestScore<seriesScores[i]){
        
 	highestScore=seriesScores[i];
-	highestScoreCount++;
+	mostRecordRuns++;
 	
 	}
 	else if(lowestScore>seriesScores[i]){
         
 	lowestScore=seriesScores[i];
- 	lowestScoreCount++;
+ 	leastRecordRuns++;
         	
 	}
 
 }
-
+console.log(mostRecordRuns+" "+leastRecordRuns);
 
